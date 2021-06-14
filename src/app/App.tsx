befbd4eb14c3752f0f8290192ea16694d7fbcc65
lajanzen <lara-janzen@hotmail.com>
2021-06-14 16:36:38 +0200

    Create Route for landing page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,55 +1,30 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import styles from "./App.module.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, RouteProps } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+
+type CustomRouteProps = RouteProps & {
+  Component: () => JSX.Element;
+  path: string;
+};
+
+const routes: CustomRouteProps[] = [
+  { path: "/", Component: LandingPage, exact: true },
+];
 
 function App(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
-
   return (
-    <Router>
-      <div className={styles.App}>
-        <header className={styles["App-header"]}>
-          <img src={logo} className={styles["App-logo"]} alt="logo" />
-          <p>Hello Vite + React!</p>
-          <p>
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is: {count}
-            </button>
-          </p>
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-          <p>
-            <a
-              className={styles["App-link"]}
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {" | "}
-            <a
-              className={styles["App-link"]}
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-          <Switch>
-            <Route path="/help">
-              <p>Help</p>
+    <div className={styles.container}>
+      <BrowserRouter>
+        <Switch>
+          {routes.map(({ Component, ...routeProps }) => (
+            <Route key={routeProps.path} {...routeProps}>
+              <Component />
             </Route>
-            <Route path="/about">
-              <p>About</p>
-            </Route>
-          </Switch>
-        </header>
-      </div>
-    </Router>
+          ))}
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
