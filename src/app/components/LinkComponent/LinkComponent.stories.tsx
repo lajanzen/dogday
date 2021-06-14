444ddfeb588f9a6b2e0f0bbae5c09d6d356148ef
lajanzen <lara-janzen@hotmail.com>
2021-06-14 18:00:55 +0200

    Change href to React Link and rename Link Component

---
+++ src/app/components/LinkComponent/LinkComponent.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import LinkComponent from "./LinkComponent";
+
+export default {
+  title: "Component/LinkComponent",
+  component: LinkComponent,
+};
+
+export const RegisterDog = (): JSX.Element => (
+  <LinkComponent link="#" variant="primary">
+    Hund registrieren
+  </LinkComponent>
+);
+
+export const RegisterSitter = (): JSX.Element => (
+  <LinkComponent link="#" variant="primary">
+    Hundesitter werden
+  </LinkComponent>
+);
+
+export const LogIn = (): JSX.Element => (
+  <LinkComponent link="#" variant="secondary">
+    Log-In
+  </LinkComponent>
+);
---
+++ src/app/components/LinkComponent/LinkComponent.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from "react";
+import { Link } from "react-router-dom";
+import styles from "./LinkComponent.module.css";
+
+type LinkComponentProps = {
+  children: ReactNode;
+  variant?: "primary" | "secondary";
+  link: string;
+};
+
+function LinkComponent({
+  children,
+  variant,
+  link,
+}: LinkComponentProps): JSX.Element {
+  return (
+    <Link
+      to={link}
+      className={
+        variant ? `${styles[`link--${variant}`]} ${styles.link}` : styles.link
+      }
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default LinkComponent;
