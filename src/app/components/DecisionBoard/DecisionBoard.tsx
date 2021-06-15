7df17437347ca66caa5a656253540d9d61277f57
lajanzen <lara-janzen@hotmail.com>
2021-06-15 15:32:15 +0200

    Add Decision Board

---
+++ src/app/components/DecisionBoard/DecisionBoard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./DecisionBoard.module.css";
+import Button from "../Button/Button";
+import YesIcon from "../Icons/YesIcon";
+import NoIcon from "../Icons/NoIcon";
+
+function DecisionBoard(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <Button variant="round-border">
+        <YesIcon />
+      </Button>
+      <Button variant="round-border">
+        <NoIcon />
+      </Button>
+    </div>
+  );
+}
+
+export default DecisionBoard;
