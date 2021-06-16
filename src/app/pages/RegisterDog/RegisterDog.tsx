d27a4ac604f5dd80efa95fd7717fc1d8776a9f18
lajanzen <lara-janzen@hotmail.com>
2021-06-16 11:15:15 +0200

    Style register page

--- src/app/pages/RegisterDog/RegisterDog.tsx
+++ src/app/pages/RegisterDog/RegisterDog.tsx
@@ -7,53 +7,55 @@ import styles from "./RegisterDog.module.css";
 
 function RegisterDog(): JSX.Element {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <BackButton />
-        <img className={styles.header__logo} src="/smallLogo.png" alt="" />
-      </header>
+    <div>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <BackButton />
+          <img className={styles.header__logo} src="/smallLogo.png" alt="" />
+        </header>
 
-      <main className={styles.main}>
-        <form className={styles.main__form}>
-          <LabeledInput
-            label="Hunde-Name"
-            id="name"
-            value=""
-            onChange={console.log}
-          />
-          <LabeledInput
-            label="Geburtsjahr des Hundes"
-            id="birth"
-            value=""
-            onChange={console.log}
-          />
-          <LabeledInput
-            label="Deine E-Mail"
-            id="mail"
-            value=""
-            onChange={console.log}
-          />
-          <LabeledInput
-            label="Deine Handynummer"
-            id="phone"
-            value=""
-            onChange={console.log}
-          />
-          <LabeledInput
-            label="Deine Stadt"
-            id="city"
-            value=""
-            onChange={console.log}
-          />
-          <LabeledInput
-            label="Passwort"
-            id="password"
-            value=""
-            onChange={console.log}
-          />
-          <Button variant="primary">Hund registrieren</Button>
-        </form>
-      </main>
+        <main className={styles.main}>
+          <form className={styles.main__form}>
+            <LabeledInput
+              label="Hunde-Name"
+              id="name"
+              value=""
+              onChange={console.log}
+            />
+            <LabeledInput
+              label="Geburtsjahr des Hundes"
+              id="birth"
+              value=""
+              onChange={console.log}
+            />
+            <LabeledInput
+              label="Deine E-Mail"
+              id="mail"
+              value=""
+              onChange={console.log}
+            />
+            <LabeledInput
+              label="Deine Handynummer"
+              id="phone"
+              value=""
+              onChange={console.log}
+            />
+            <LabeledInput
+              label="Deine Stadt"
+              id="city"
+              value=""
+              onChange={console.log}
+            />
+            <LabeledInput
+              label="Passwort"
+              id="password"
+              value=""
+              onChange={console.log}
+            />
+            <Button variant="primary">Hund registrieren</Button>
+          </form>
+        </main>
+      </div>
       <footer className={styles.footer}>
         <DogIcon />
       </footer>
