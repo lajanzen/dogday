d9acc3023b3d971ed2f737fcc1739a4c7f0ab216
lajanzen <lara-janzen@hotmail.com>
2021-06-18 16:25:13 +0200

    Add SearchSitter page

---
+++ src/app/pages/SearchSitter/SearchSitter.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchSitter from "./SearchSitter";
+
+export default {
+  title: "Page/SearchSitter",
+  component: SearchSitter,
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export const Jana = (): JSX.Element => <SearchSitter />;
