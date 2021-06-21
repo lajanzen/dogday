f2a215eff6d03438ede4d59b64115c13fb610e23
lajanzen <lara-janzen@hotmail.com>
2021-06-21 16:38:59 +0200

    Add user types

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export type UserSitter = {
+  name: string;
+  email: string;
+  phone: string;
+  city: string;
+  experience: string;
+  password: string;
+  photo: string;
+};
+
+export type UserDog = {
+  name: string;
+  birth: number;
+  email: string;
+  phone: string;
+  city: string;
+  password: string;
+  photo: string;
+};
