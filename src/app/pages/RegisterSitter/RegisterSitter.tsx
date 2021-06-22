8b88d88bbdeff88a3a8ff0c41f5f83d608c33976
lajanzen <lara-janzen@hotmail.com>
2021-06-22 16:41:07 +0200

    Send register form to MongoDB

--- src/app/pages/RegisterSitter/RegisterSitter.tsx
+++ src/app/pages/RegisterSitter/RegisterSitter.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import BackButton from "../../components/BackButton/BackButton";
 import Button from "../../components/Button/Button";
 import DropdownMenu from "../../components/DropdownMenu/DropdownMenu";
 import DogIcon from "../../components/Icons/DogIcon";
 import LabeledInput from "../../components/LabeledInput/LabeledInput";
 import styles from "./RegisterSitter.module.css";
+import { UserSitter } from "../../../types";
+import { postUser } from "../../api/users";
+import { useHistory } from "react-router-dom";
 
 function RegisterSitter(): JSX.Element {
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState(0);
+  const [city, setCity] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const user: UserSitter = { name, email, phone, city, password };
+    postUser(user);
+    history.push("/");
+  }
+
   return (
     <div>
       <div className={styles.container}>
@@ -16,37 +34,47 @@ function RegisterSitter(): JSX.Element {
         </header>
 
         <main className={styles.main}>
-          <form className={styles.main__form}>
+          <form className={styles.main__form} onSubmit={handleSubmit}>
             <LabeledInput
               label="Dein Name"
-              id="dogName"
-              value=""
-              onChange={console.log}
+              type="text"
+              id="name"
+              value={name}
+              onChange={setName}
+              required
             />
             <LabeledInput
               label="Deine E-Mail"
+              type="email"
               id="email"
-              value=""
-              onChange={console.log}
+              value={email}
+              onChange={setEmail}
+              required
             />
             <LabeledInput
               label="Deine Handynummer"
+              type="number"
               id="phone"
-              value=""
-              onChange={console.log}
+              value={phone}
+              onChange={(value) => setPhone(+value)}
+              required
             />
             <LabeledInput
               label="Deine Stadt"
+              type="text"
               id="city"
-              value=""
-              onChange={console.log}
+              value={city}
+              onChange={setCity}
+              required
             />
             <DropdownMenu />
             <LabeledInput
               label="Passwort"
+              type="password"
               id="password"
-              value=""
-              onChange={console.log}
+              value={password}
+              onChange={setPassword}
+              required
             />
             <Button variant="primary">Registrieren</Button>
           </form>
