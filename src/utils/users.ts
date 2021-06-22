97c9ae9dcd8dc66536d3a5851a94d89fa7621283
lajanzen <lara-janzen@hotmail.com>
2021-06-22 13:54:58 +0200

    Add readUser function

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -4,3 +4,17 @@ import { getUsersCollection } from "./database";
 export const readUsers = async (): Promise<UserSitter[]> => {
   return await getUsersCollection().find().toArray();
 };
+
+export const readUser = async (
+  email: string
+): Promise<UserSitter | undefined> => {
+  const user = await getUsersCollection().findOne({ email });
+  if (!user) {
+    throw new Error("There is no user with that email adress");
+  }
+  return user;
+};
+
+export const saveUser = async (newUser: UserSitter): Promise<void> => {
+  await getUsersCollection().insertOne({ ...newUser });
+};
