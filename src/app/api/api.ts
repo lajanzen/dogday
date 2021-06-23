2abdf6e69866daf0b8e757b1eee7ff5082e02162
lajanzen <lara-janzen@hotmail.com>
2021-06-23 14:58:17 +0200

    Rename users.ts to api.ts

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -15,3 +15,21 @@ export async function postUser(user: UserSitter): Promise<UserSitter> {
   const result: UserSitter = await response.json();
   return result;
 }
+
+export async function verifyLogin(
+  user: Partial<UserSitter>
+): Promise<UserSitter> {
+  const response = await fetch("/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw errorMessage;
+  }
+  const result: UserSitter = await response.json();
+  return result;
+}
