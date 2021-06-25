150dc0f0c19e8b0301ebf0a3d14dff5a129a7de3
lajanzen <lara-janzen@hotmail.com>
2021-06-25 18:26:29 +0200

    Add Matches page

---
+++ src/app/pages/Matches/Matches.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { UserDog, UserSitter } from "../../../types";
+import DogIcon from "../../components/Icons/DogIcon";
+import MatchCard from "../../components/MatchCard/MatchCard";
+import NavBar from "../../components/NavBar/NavBar";
+import styles from "./Matches.module.css";
+
+function Matches(): JSX.Element {
+  const [users, setUsers] = useState<UserSitter[] | UserDog[]>([]);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((response) => response.json())
+      .then((users) => setUsers(users));
+  }, []);
+
+  function age(birthYear: number): number {
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+  }
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <NavBar />
+      </header>
+      <main className={styles.main}>
+        {users.map((user) => (
+          <MatchCard
+            imgSrc={user.photo}
+            name={user.name}
+            mail={user.email}
+            phone={user.phone}
+            info={`${age(user.birthYear)}`}
+          />
+        ))}
+        <footer className={styles.footer}>
+          <DogIcon />
+        </footer>
+      </main>
+    </div>
+  );
+}
+
+export default Matches;
