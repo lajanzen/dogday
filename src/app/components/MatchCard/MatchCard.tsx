5a082b14cfa2130f819c5ccc6ceb8a2bb45091e2
lajanzen <lara-janzen@hotmail.com>
2021-06-29 12:36:53 +0200

    Add user type to match card and make info more dynamic

--- src/app/components/MatchCard/MatchCard.tsx
+++ src/app/components/MatchCard/MatchCard.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import styles from "./MatchCard.module.css";
 
 export type MatchCardProps = {
-  imgSrc: string;
+  imgSrc?: string;
   name: string;
   info: string;
   mail: string;
   phone: string;
+  type: string;
 };
 
 function MatchCard({
@@ -15,13 +16,14 @@ function MatchCard({
   info,
   mail,
   phone,
+  type,
 }: MatchCardProps): JSX.Element {
   return (
     <article className={styles.container}>
       <img className={styles.image} src={imgSrc} alt="Profile Image" />
       <div className={styles.info}>
         <span className={styles.info__name}>{name}</span>
-        <span>{info} Jahre alt</span>
+        <span>{type === "dog" ? `${info} Jahre alt` : info}</span>
         <span>E-Mail: {mail}</span>
         <span>Telefon: {phone}</span>
       </div>
